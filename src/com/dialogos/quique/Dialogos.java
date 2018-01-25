b53239029cc9d303999b9318b1fc8a799b80821c
Enrique Alvarez <ealvarezpereira@danielcastelao.org>
2018-01-25 09:16:30 +0100

    Añadido metodo para preguntar Si/No

--- src/com/dialogos/quique/Dialogos.java
+++ src/com/dialogos/quique/Dialogos.java
@@ -19,7 +19,13 @@ public class Dialogos {
         JOptionPane.showMessageDialog(null, recibirMensaje);
     }
 
-    
+    public static boolean sioNO(String pregunta, String cabecera) {
+        int dialogButton = JOptionPane.showConfirmDialog(null, pregunta, cabecera, JOptionPane.YES_NO_OPTION);
+        if (dialogButton == JOptionPane.YES_OPTION) {
+            return true;
+        } else {
+            return false;
+        }
+    }
    
-
 }
